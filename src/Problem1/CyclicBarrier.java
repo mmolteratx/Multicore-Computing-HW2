c7e422dff659f70b0a44a857c422537e390f9b49
mmolteratx <32653343+mmolteratx@users.noreply.github.com>
2020-10-11 16:16:26 -0500

    Bug fix

--- src/Problem1/CyclicBarrier.java
+++ src/Problem1/CyclicBarrier.java
@@ -1,10 +1,12 @@
 package Problem1;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CyclicBarrier {
 
     Semaphore sema;
+    AtomicBoolean rel = new AtomicBoolean(false);
 
     public CyclicBarrier(int parties) {
         sema = new Semaphore(parties);
@@ -14,9 +16,11 @@ public class CyclicBarrier {
         sema.acquire();
         int pos = sema.availablePermits();
 
-        while (sema.availablePermits() > 0) {
+        while (sema.availablePermits() > 0 && !rel.get()) {
         }
-
+        
+        if(!rel.get()) {rel.set(true)};
+        
         sema.release();
         return pos;
     }
